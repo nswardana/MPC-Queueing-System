d10534daa3c2dc43d1939d0bf988bae899d1d9d8
nswardana <nswardana@gmail.com>
2024-11-04 16:27:00 +0700

    up

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,5 +1,5 @@
-//var URL = "https://antrianapi.armonia.beeasy.id";
-var URL = "http://localhost:4002";
+var URL = "https://antrianapi.armonia.beeasy.id";
+//var URL = "http://localhost:4002";
 export const config = {
 	URL
 };
